2dcc4ddb9e80f376077338a0e31f849bfc43e6c5
Malkieg <malkieg970@gmail.com>
2019-03-13 16:32:24 -0400

    End of part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,36 @@
 import {Component, OnInit} from '@angular/core';
+import {Restaurant} from './models/restaurant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Kosher';
-  cardTitle = 'Kosher';
-  cardSubtitle = 'an app to show kosher things';
-  cardContent = 'some stuff that we will fill in later';
-  // Init method
-  ngOnInit(): void {
-    this.cardTitle = 'WORD!!!';
+  rests: Restaurant[];
+  constructor() {
+    this.rests = [];
+    let temp = new Restaurant();
+    temp.name = 'Tav Cafe';
+    temp.phone = '(514) 112-3581';
+    temp.rating = 3;
+    temp.priceRange = 3;
+    temp.website = 'http://cafetav.com/';
+    this.rests.push(temp);
+
+    temp = new Restaurant();
+    temp.name = 'Pizza Pita';
+    temp.phone = '(514) 231-1111';
+    temp.rating = 4;
+    temp.priceRange = 2;
+    this.rests.push(temp);
+
+    temp = new Restaurant();
+    temp.name = 'Mozart';
+    temp.rating = 2;
+    temp.priceRange = 2;
+    temp.website = 'http://restomozart.com/';
+    this.rests.push(temp);
   }
 }
