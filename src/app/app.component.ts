ad490ded41af6cc0ff1c8e986db2521e1723d53e
Malkieg <malkieg970@gmail.com>
2019-03-13 13:38:41 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Kosher';
+  cardTitle = 'Kosher';
+  cardSubtitle = 'an app to show kosher things';
+  cardContent = 'some stuff that we will fill in later';
+  // Init method
+  ngOnInit(): void {
+    this.cardTitle = 'WORD!!!';
+  }
 }
